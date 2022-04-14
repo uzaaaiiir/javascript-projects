1749ebbb68c75c35023215e3ff033a43c4a4ea9f
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-14 00:12:05 -0400

    Tip Calc

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,24 +1,45 @@
 "use strict";
 
-// Input Data by User
-totalBill = document.querySelector("#bill");
-tipPercent = document.querySelector("#tip");
-numberOfPeople = document.querySelector("#people");
-
 // Element to Output
-total = document.querySelector(".total");
-tip = document.querySelector(".tip");
+const total = document.querySelector(".total");
+const tip = document.querySelector(".tip");
 
 // Calculate Button
-calculateBtn = document.querySelector(".btn");
+const form = document.querySelector(".input-section");
+console.log(form);
+const calculateBtn = document.querySelector(".input-section");
+// const submitBtn = document.getElementById("submit");
+
+// Input Data by User
+let totalBill = form.elements[0];
+let tipPercent = form.elements[1];
+let numberOfPeople = form.elements[2];
+console.log(totalBill);
+console.log(tipPercent);
+// const totalBill = document.querySelector("#bill");
+// const tipPercent = document.querySelector("#tip");
+// const numberOfPeople = document.querySelector("#people");
 
 // Calculate Total Tip
 const calculateTip = function () {
-    return (tipPercent.value / 100) * totalBill.value;
+    return (Number(tipPercent.value) / 100) * Number(totalBill.value);
 };
 
 // Calculate Total Bill Per Person
 const calculateTotalPerPerson = function () {
     const tipAmount = calculateTip();
-    return (tipAmount + totalBill.value) / numberOfPeople.value;
+    return (tipAmount + Number(totalBill.value)) / Number(numberOfPeople.value);
 };
+
+const output = function () {
+    console.log(totalBill.value, tipPercent.value);
+    const tipTotal = calculateTip();
+    console.log(tipTotal);
+    const totalAmount = calculateTotalPerPerson();
+    console.log(totalAmount);
+    total.textContent = totalAmount;
+    tip.textContent = tipTotal;
+};
+
+calculateBtn.addEventListener("click", output);
+// submitBtn.addEventListener("click", output);
