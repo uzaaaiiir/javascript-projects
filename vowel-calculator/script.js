d7504d5c6cd499116716e48265365ee92b02ba21
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-17 17:14:02 -0400

    Vowel Calc

--- vowel-calculator/script.js
+++ vowel-calculator/script.js
@@ -1,18 +1,35 @@
-const textInput = document.getElementById("string");
+const form = document.querySelector(".input-section");
+const textInput = form.elements[0];
 const btn = document.querySelector(".btn");
 const vowels = document.querySelector(".vowels");
 
 function countVowels() {
     let count = 0;
-    const textEntered = String(textInput.Value);
+    const textEntered = textInput.value;
     // console.log(textInput.value, textInput.value.length);
 
+    // console.log(textInput.value);
+
     for (let i = 0; i < textEntered.length; i++) {
-        if ("aeiouAEIOU".indexOf(textEntered[i]) != -1) {
+        // if ("aeiouAEIOU".indexOf(textEntered[i]) != -1) {
+        if (
+            textEntered[i] === "a" ||
+            textEntered[i] === "e" ||
+            textEntered[i] === "i" ||
+            textEntered[i] === "o" ||
+            textEntered[i] === "u" ||
+            textEntered[i] === "A" ||
+            textEntered[i] === "E" ||
+            textEntered[i] === "I" ||
+            textEntered[i] === "O" ||
+            textEntered[i] === "U"
+        ) {
             count++;
+            // console.log(textEntered[i], count);
         }
     }
     // console.log(vowels);
+    // console.log(count);
 
     vowels.textContent = count;
 }
